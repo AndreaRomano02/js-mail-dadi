8669b634aa77a81fb6f59ec6256f25df65001559
AndreaRomano02 <andy.romano2002@gmail.com>
2023-05-31 16:40:54 +0200

    Add main Structur for dadi

--- js/dadi.js
+++ js/dadi.js
@@ -1,1 +1,19 @@
-console.log('JS OK');
+console.log("JS OK");
+
+//# Variabili
+let message = "Ha perso!!!";
+
+//# Dichiaro le costanti per il random
+const randomUser = parseInt(Math.floor(Math.random() * 6) + 1);
+const randomPC = parseInt(Math.floor(Math.random() * 6) + 1);
+
+console.log("user number: " + randomUser, "pc number: " + randomPC);
+
+//# Controllo se sono uguali
+if (randomPC === randomUser) message = "Pareggio!!!";
+
+//# Controllo a chi è uscito il numero maggiore
+if (randomUser > randomPC) message = "Hai vinto!!!";
+
+//# Stampo il messaggio
+console.log(message);
